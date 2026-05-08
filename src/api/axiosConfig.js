38c843672e09e88cdf17c688e65d4ada7e2487bc
mikky <mikky123@gmail.com>
2026-05-08 19:18:27 +0100

    modified-file

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://aaf2b248436f04c76be85227a9f32175-1821011344.us-east-1.elb.amazonaws.com:8080',
+    baseURL:'http://13.51.251.187:8080',
+
     headers: {
         'Content-Type': 'application/json',
     },
