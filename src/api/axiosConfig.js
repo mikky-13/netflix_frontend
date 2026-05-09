ef7b0fb9e00652682b27dade30e2ea647f9795e8
mikky <mikky123@gmail.com>
2026-05-09 01:14:23 +0100

    modified-file

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://16.16.169.173:8080',
+    baseURL:'http://16.170.166.63:8080',
 
     headers: {
         'Content-Type': 'application/json',
